6f84737d7ad7db852b787042f4e5360bd918fff0
Code Queen <channguyen97@gmail.com>
2023-10-24 16:06:17 -0700

    updated pseudocode

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file. Done
-// TODO: Add CSS loaders and babel to webpack. Done
+// Add and configure workbox plugins for a service worker and manifest file
+//Add CSS loaders and babel to webpack
 
 module.exports = () => {
   return {
@@ -34,7 +34,7 @@ module.exports = () => {
         publicPath: './',
         icons: [
           {
-            src: path.resolve('images/logo.png'),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('images', 'icons'),
           },
