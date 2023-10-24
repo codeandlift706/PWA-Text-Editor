69850854133a7efe0956cde7168f4e7dfa7d87af
Code Queen <channguyen97@gmail.com>
2023-10-24 15:54:29 -0700

    added to install file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// TODO: Add logic to a method that accepts some content and adds it to the database. Done
 export const putDb = async (content) => {
   console.log('PUT to the database');
   const textDb = await openDB('texts', 1);
